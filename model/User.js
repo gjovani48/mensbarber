3322d7fb6a635481c1c7782da2908957b61dd8c3
gjovani48 <53860258+gjovani48@users.noreply.github.com>
2019-11-27 11:48:24 +0800

    Update User.js

--- model/User.js
+++ model/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/salon',{useNewUrlParser:true, useUnifiedTopology: true});
-
+//mongoose.connect('mongodb://localhost:27017/salon',{useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://admin:admin@cluster0-er17b.mongodb.net/test?retryWrites=true&w=majority
+',{useNewUrlParser:true, useUnifiedTopology: true});
+                 
 const User = mongoose.model('User',{
 
     firstname: {
